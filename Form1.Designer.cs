957160b0fda922426e1cf81b12893c1821fc1e6f
Alla Kokhaniuk <121404468+Alla-Ko@users.noreply.github.com>
2023-11-04 17:47:26 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace WindowsFormsApp_time_231104
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_start = new System.Windows.Forms.Button();
+            this.button_end = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_start
+            // 
+            this.button_start.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button_start.Location = new System.Drawing.Point(8, 171);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(141, 23);
+            this.button_start.TabIndex = 0;
+            this.button_start.Text = "start";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // button_end
+            // 
+            this.button_end.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button_end.Location = new System.Drawing.Point(155, 171);
+            this.button_end.Name = "button_end";
+            this.button_end.Size = new System.Drawing.Size(140, 23);
+            this.button_end.TabIndex = 1;
+            this.button_end.Text = "stop";
+            this.button_end.UseVisualStyleBackColor = true;
+            this.button_end.Click += new System.EventHandler(this.button_end_Click);
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 90F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(448, 135);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "00:00";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(301, 171);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "cancel";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 201);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_end);
+            this.Controls.Add(this.button_start);
+            this.MaximumSize = new System.Drawing.Size(464, 240);
+            this.MinimumSize = new System.Drawing.Size(464, 240);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Timer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Button button_end;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using System.Diagnostics.Metrics;
+
+namespace WindowsFormsApp_time_231104
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            a.Interval = 1000;
+            a.Tick += new EventHandler(TimerEventProcessor);
+            button_end.Enabled = false;
+            button_start.Enabled = true;
+        }
+        public int seconds = 1, minutes = 0;
+        public Timer a=new Timer();
+        private void TimerEventProcessor (Object sender, EventArgs e)
+        {
+            if (seconds < 10 && minutes < 10)
+            {
+                label1.Text="0"+minutes.ToString()+":0"+seconds.ToString();
+            }
+            else if (seconds < 10)
+            {
+                label1.Text=minutes.ToString()+ ":0" + seconds.ToString();
+            }
+            else if (minutes < 10)
+            {
+                label1.Text="0"+minutes.ToString()+":"+seconds.ToString();
+            }
+            seconds++;
+            if (seconds == 60)
+            {
+                minutes++;
+                seconds= 0;
+            }
+        }
+
+
+
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            a.Start();
+            button_end.Enabled = true;
+            button_start.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            seconds=0;
+            minutes=0;
+            label1.Text = "00:00";
+        }
+
+        private void button_end_Click(object sender, EventArgs e)
+        {
+            a.Stop();
+            button_start.Enabled = true;
+            button_end.Enabled = false;
+            
+        }
+    }
+}
